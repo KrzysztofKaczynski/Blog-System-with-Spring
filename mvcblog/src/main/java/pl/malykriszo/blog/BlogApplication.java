3546a2be37619b8ff663fbba8fdca7b64452895f
KrzysztofKaczynski <qris70@gmail.com>
2018-08-21 14:04:50 +0200

    [add] Spring Boot Application Class

---
+++ mvcblog/src/main/java/pl/malykriszo/blog/BlogApplication.java
@@ -0,0 +1,15 @@
+package pl.malykriszo.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * pl.malykriszo.blog Created by MałyKriszo on 2018-08-21.
+ */
+
+@SpringBootApplication
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+}
