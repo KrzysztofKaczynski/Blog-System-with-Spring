89046f7269e7ee6bab0d6dfb8d577cbeeee69492
KrzysztofKaczynski <qris70@gmail.com>
2018-08-23 18:03:37 +0200

    [add] User and Post

---
+++ mvcblog/src/main/java/pl/malykriszo/blog/models/Post.java
@@ -0,0 +1,73 @@
+package pl.malykriszo.blog.models;
+
+import java.util.Date;
+
+public class Post {
+
+    private Long id;
+    private String title;
+    private String contents;
+    private User author;
+    private Date date = new Date();
+
+    public Post() {
+    }
+
+    public Post(Long id, String title, String contents, User author) {
+        this.id = id;
+        this.title = title;
+        this.contents = contents;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", contents='" + contents + '\'' +
+                ", author=" + author +
+                ", date=" + date +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
---
+++ mvcblog/src/main/java/pl/malykriszo/blog/models/User.java
@@ -0,0 +1,74 @@
+package pl.malykriszo.blog.models;
+
+import pl.malykriszo.blog.models.Post;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class User {
+
+    private Long id;
+    private String username;
+    private String password;
+    private String fullName;
+    private Set<Post> posts = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(Long id, String username, String fullName) {
+        this.id = id;
+        this.username = username;
+        this.fullName = fullName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", fullName='" + fullName + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Set<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(Set<Post> posts) {
+        this.posts = posts;
+    }
+}
