dfb15545ece640ac329a5753f2f31bf34700785e
KrzysztofKaczynski <qris70@gmail.com>
2018-08-28 18:10:58 +0200

    [add] PostController

---
+++ mvcblog/src/main/java/pl/malykriszo/blog/controllers/PostController.java
@@ -0,0 +1,22 @@
+package pl.malykriszo.blog.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.malykriszo.blog.models.Post;
+import pl.malykriszo.blog.services.PostService;
+
+@Controller
+public class PostController {
+    @Autowired
+    private PostService postService;
+
+    @RequestMapping("/posts/view/{id}")
+    public String view(@PathVariable("id") Long id, Model model) {
+        Post post = postService.findById(id);
+        model.addAttribute("post", post);
+        return "posts/view";
+    }
+}
