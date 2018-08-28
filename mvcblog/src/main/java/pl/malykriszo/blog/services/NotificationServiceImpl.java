621c763d144e4515db0e99b72db75dc778effdcb
KrzysztofKaczynski <qris70@gmail.com>
2018-08-28 20:36:24 +0200

    [add] NotificationService

---
+++ mvcblog/src/main/java/pl/malykriszo/blog/services/NotificationServiceImpl.java
@@ -0,0 +1,15 @@
+package pl.malykriszo.blog.services;
+
+public class NotificationServiceImpl implements NotificationService {
+
+
+    @Override
+    public void addInfoMessage(String msg) {
+
+    }
+
+    @Override
+    public void addErrorMessage(String msg) {
+
+    }
+}
