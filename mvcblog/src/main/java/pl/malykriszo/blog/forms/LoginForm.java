052e4489d9a82b62dadef4a74d65e9ebb0240ac0
KrzysztofKaczynski <qris70@gmail.com>
2018-08-30 13:09:47 +0200

    [add] LoginForm

---
+++ mvcblog/src/main/java/pl/malykriszo/blog/forms/LoginForm.java
@@ -0,0 +1,33 @@
+package pl.malykriszo.blog.forms;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * pl.malykriszo.blog.forms Created by MałyKriszo on 2018-08-30.
+ */
+public class LoginForm {
+
+    @Size(min = 2, max = 30, message = "Nazwa użytkownika powinna znajdować się w przedziale [2 ... 30]")
+    private String username;
+
+    @NotNull
+    @Size(min = 1, max = 50)
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
